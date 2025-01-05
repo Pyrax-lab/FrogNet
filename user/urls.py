e62feea6debdfd84152fbe5564a5ace56b3a0a17
Artur <proxaker1111@gmail.com>
2025-01-05 13:47:10 +0200

    login and register form redisyne

--- user/urls.py
+++ user/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     #path('register/', views.RegisterUser.as_view(), name = "register"),
     path('register/', views.register, name="register"),
     path("profile/", views.profile, name="profile"),
+    path("profile/<int:id>", views.profile, name="profile"),
     path("change_profile/", views.change_profile, name="change_profile"),
 ]
